d6e3bc825a8fea6d6e8f8b819eb67a6e8614f6a6
Agorablood <eli0002@alu.medac.es>
2024-01-31 12:18:19 +0100

    link de css-html

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,15 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="{{ asset('/img/ares.jpg') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('img/ares.jpg') }}">
     <link rel="shortcut icon" sizes="192x192" href="{{ asset('img/ares.jpg') }}">
-    
+    <link rel="stylesheet" href="{!! asset('css/estilos.css') !!}">
     <title>Ares</title>
+    <style>
+        
+    </style>
 </head>
+
 <body>
-    <h1>ARES</h1>
-    
+
+    <body>
+        <div id="particles-js" class="particles"></div>
+
+        <div class="game-container">
+            <h1>EMPIEZA TU HISTORIA</h1>
+            <img id="movieImage" src="" alt="Ares" />
+            
+        </div>
+
+        <div id="gameOverModal" class="modal">
+            <div class="modal-content"></div>
+        </div>
+    </body>
+
 </body>
+
 </html>
